ea84ffb5682785fe46cdbb947e52f137e6da2106
Demian (FAU) <ict1986@gmail.com>
2013-06-21 15:09:42 +0200

    add delete option to rsync

--- home/scripts/rsync-backup.sh
+++ home/scripts/rsync-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd
-rsync -av --exclude-from scripts/rsync-exclude.txt lehre wemucs Bilder toybox i2s:backups
+rsync -av --delete --exclude-from scripts/rsync-exclude.txt lehre wemucs Bilder toybox i2s:backups &>>/tmp/rsyncbackup.log
